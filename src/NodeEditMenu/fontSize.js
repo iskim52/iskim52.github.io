f21db82f65b10d73776efdc1964682e6d0c62e98
devien <devien@DESKTOP-7B7T1DP.localdomain>
2022-11-22 23:13:43 -0500

    impl font size picker

--- src/NodeEditMenu/fontSize.js
+++ src/NodeEditMenu/fontSize.js
@@ -1,18 +1,51 @@
-
+import React, { useState } from 'react';
+import ReactDOMServer from 'react-dom/server'
 import Select from 'react-select';
+import HTMLString from 'react-html-string';
+
+import useStore from '../store.tsx';
+
+import '../css/nodeedit.css'
+
 
 const fontSize = [8,9,10,11,12,14,18,24,30,36,48,60,72,96]
 const fontSizeObj = fontSize.map(fontSize => {
   return {label:fontSize, value:fontSize}
 });
 
+
+
 export default function FontSize() { 
+
+  const [selectedOption, ] = useState(null);
+  const {getNode, clickednode, setNodeHtml} = useStore();
+
+  function selectOption(evt) {
+    let node = getNode(clickednode)
+    let htmlString = node.data.htmlData
+    let html;
+
+    if (node.data.htmlData.indexOf('font-size') > -1) {
+      const regex = /(\d+)px/;
+      html = htmlString.replace(regex, String(evt.label)+'px')
+      html = <HTMLString html={html} />
+    }
+    else {
+      html = <HTMLString html={htmlString}/>
+      html = <div style={{fontSize:evt.label}}>{html}</div>
+    }
+    
+    let htmlRepl = ReactDOMServer.renderToString(html)
+    setNodeHtml(node, htmlRepl);
+  }
+
   return(
     <Select 
     options={fontSizeObj} 
     placeholder="8"
     defaultValue={8} //set default value to selected font
-    // onChange={selectOption}
+    onChange={selectOption}
+    // onClick={(e) => console.log(e)}
     // isSearchable={true}
     styles={{
       control: (provided, state) => ({
